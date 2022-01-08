f7886b010bce11a0ea2d2ff7d5db97263d8dee16
Renato Boucas <renatoboucas@gmail.com>
2022-01-08 00:25:56 -0500

    Tested all routing, navigation, authentication with firebase, hook, recover auth state and auth context.

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,10 +1,16 @@
+import { Link} from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
+
 import '../styles/auth.scss' 
 import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
+
 
 export function NewRoom() {
+    // const { user } = useAuth;
+
     return (
         <div id="page-auth">
             <aside>
@@ -13,6 +19,7 @@ export function NewRoom() {
                 <p>Clear your audience's doubts in real-time</p>
             </aside>
             <main>
+                
                 <div className="main-content">
                     <img src={logoImg} className="logo" alt="Letmeask" />
                     <h2>Create a new room</h2>
@@ -22,7 +29,7 @@ export function NewRoom() {
                             Create room
                         </Button>
                     </form>
-                    <p>Do you want to join an existing room? <a href="#">Click here</a></p>
+                    <p>Do you want to join an existing room? <Link to="/">Click here</Link></p>
                 </div>
             </main>
         </div>
